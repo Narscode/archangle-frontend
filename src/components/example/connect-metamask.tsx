b4c7070c7f2c4b1bcd3ba686ea0aded209fd703a
alexchtst <acinatra@gmail.com>
2025-08-15 04:25:20 +0700

    feat: initialize login with google example and using smartcontract implementation + connect metamask wallet

---
+++ src/components/example/connect-metamask.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import lockContract from "@/utils/Lock.json";
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+export default function ConnectMetaMask() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        connectContract();
+    }, []);
+
+    async function connectContract() {
+        // null safety
+        if (!CONTRACT_ADDRESS) return null;
+
+        if (typeof window.ethereum !== "undefined") {
+            await (window as any).ethereum.request({ method: "eth_requestAccounts" });
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            const signer = await provider.getSigner();
+            const contract = new ethers.Contract(CONTRACT_ADDRESS, lockContract.abi, signer);
+
+            const value = await contract.getMessage();
+            setMessage(value);
+        }
+    }
+
+    <div>
+        <h1 className="font-inter">Smart Contract with adress {CONTRACT_ADDRESS}</h1>
+        <p>message {message}</p>
+    </div>
+
+}
